9b19ded00486566f42e6dbbc7c25f3770882c527
zerhud <zerhud@gmail.com>
2022-05-06 09:00:15 +0300

    delete old response parser, add definitions to http_parser.hpp for acceptor

--- http_parser.hpp
+++ http_parser.hpp
@@ -7,30 +7,61 @@
  * See accompanying file LICENSE (at the root of this repository)
  *************************************************************************/
 
+#include "http_parser/utils/pos_string_view.hpp"
 #include "http_parser/request_generator.hpp"
-#include "http_parser/response_parser.hpp"
 #include "http_parser/uri_parser.hpp"
+#include "http_parser/acceptor.hpp"
 
 namespace http_parser {
+
+constexpr std::size_t default_max_body_size = 4 * 1024;
+constexpr std::size_t default_max_head_size = 1 * 1024;
 
 namespace pmr_vec {
 using data_type = std::pmr::vector<std::byte>;
 using uri_parser = http_parser::basic_uri_parser<std::string_view>;
-using response_parser = http_parser::basic_response_parser<data_type>;
 using request_generator = http_parser::basic_request_generator<data_type, std::string_view>;
+
+template<typename Head>
+struct http1_acceptor_traits : http_parser::http1_acceptor_traits<Head, data_type> {
+	std::pmr::memory_resource* mem;
+	data_type create_data_container() override { data_type{mem}; }
+	std::pmr::vector<header_view<data_type>> create_headers_container() override
+	{ std::pmr::vector<header_view<data_type>>{mem}; }
+};
+
+template<std::size_t max_body_size = default_max_body_size, std::size_t max_head_size = default_max_head_size>
+using http1_req_acceptor = http_parser::http1_req_acceptor<std::pmr::vector, data_type, max_body_size, max_head_size>;
+
+template<std::size_t max_body_size = default_max_body_size, std::size_t max_head_size = default_max_head_size>
+using http1_resp_acceptor = http_parser::http1_resp_acceptor<std::pmr::vector, data_type, max_body_size, max_head_size>;
+
 } // namespace pmr_vec
 
 namespace pmr_str {
 using data_type = std::pmr::string;
 using uri_parser = http_parser::basic_uri_parser<std::string_view>;
 using uri_wparser = http_parser::basic_uri_parser<std::wstring_view>;
-using response_parser = http_parser::basic_response_parser<data_type>;
 using request_generator = http_parser::basic_request_generator<data_type, std::string_view>;
+
+template<typename Head>
+struct http1_acceptor_traits : http_parser::http1_acceptor_traits<Head, data_type> {
+	std::pmr::memory_resource* mem;
+	data_type create_data_container() override { data_type{mem}; }
+	std::pmr::vector<header_view<data_type>> create_headers_container() override
+	{ std::pmr::vector<header_view<data_type>>{mem}; }
+};
+
+template<std::size_t max_body_size = default_max_body_size, std::size_t max_head_size = default_max_head_size>
+using http1_req_acceptor = http_parser::http1_req_acceptor<std::pmr::vector, data_type, max_body_size, max_head_size>;
+
+template<std::size_t max_body_size = default_max_body_size, std::size_t max_head_size = default_max_head_size>
+using http1_resp_acceptor = http_parser::http1_resp_acceptor<std::pmr::vector, data_type, max_body_size, max_head_size>;
+
 } // namespace pmr_str
 
 using pmr_vec::data_type;
 using pmr_vec::uri_parser;
-using pmr_vec::response_parser;
 using pmr_vec::request_generator;
 
 using pos_string_view = basic_position_string_view<std::pmr::string>;
